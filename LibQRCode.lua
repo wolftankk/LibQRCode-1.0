3f03a49ba6d481d6a1b83c45fd6ddc8dba73b316
wolftankk <wolftankk@eb97d260-d3d2-4885-ab86-618bba4096ac>
2010-09-28 06:37:17 +0000

    rewrite methods of the qrcode base class, add .docmeta
    git-svn-id: svn+ssh://svn.wowace.com/wow/libqrcode/mainline/trunk@5 eb97d260-d3d2-4885-ab86-618bba4096ac

--- LibQRCode.lua
+++ LibQRCode.lua
@@ -30,8 +30,25 @@ end
 
 local QRCODE_MATRIX = 21;--version 1, 21* 21, 4 modules increases whenever 1 version increases.
 local QRCODE_MATRIX_PER_VERSION = 4;
+local NUM_MASK_PATTERNS = 8;
+
+--@class qrcode
 local qrcode = {}
 local qrcode_MT = {__index = qrcode}
+
+--@class matrix method
+local cmatrix = {}
+local cmatrix_MT = {__index = cmatrix};
+
+--the original table is defined in the table 5 of JISX0510:2004 (p19)
+local ALPHANUMBERIC_TABLE = {
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, --0x00-0x0f
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, --0x10-0x1f
+    36, -1, -1, -1, 37, 38, -1, -1, -1, -1, 39, 40, -1, 41, 42, 43,  -- 0x20-0x2f
+    0,   1,  2,  3,  4,  5,  6,  7,  8,  9, 44, -1, -1, -1, -1, -1,  -- 0x30-0x3f
+    -1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,  -- 0x40-0x4f
+    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, -1, -1, -1, -1, -1   -- 0x50-0x5f
+}
 
 function lib:New()
     local builder = setmetatable({}, qrcode_MT); 
@@ -39,46 +56,144 @@ function lib:New()
     return builder;
 end
 
---@param level String/Int, if type is string, val: S, M, L
---if type is int, range 1 to 40;
-function qrcode:Create(str, level)
-    local ilevel = 1;
-    if type(level) == "string" then
-        local size = level:lower();
-        if size == "s" then
-            ilevel = 1;
-        elseif size == "m" then
-            ilevel = 20;
-        elseif size == "l" then
-            ilevel = 40;
-        else
-            error("Error: Level is invalid param. You can type S, M, L or number(1~40 range).", 2);
-        end
-    elseif type(level) == "number" and (level >= 1 and level <= 40) then
-        ilevel = level;
-    else
-        error("Error: Level is invalid param. You can type S, M, L or number(1~40 range).", 2);
-    end
-
-    local matrix = 0 
-    if ilevel == 1 then
-        matrix = QRCODE_MATRIX;
-    else
-        matrix = QRCODE_MATRIX + QRCODE_MATRIX_PER_VERSION * (ilevel - 1); 
-    end
-    self.matrix = matrix;
-    self:Parse(str);
+--reset qrcode params
+--@usage local a = LibStub("LibQRCode-1.0"); a:reset();
+function qrcode:reset()
+    self.mode = nil;
+    self.ecLevel = nil;
+    self.version = -1;
+    self.matrixWidth = -1;
+    self.maskPattern = -1;
+    self.numTotalBytes = -1;
+    self.numDataBytes = -1;
+    self.numECBytes = -1;
+    self.numRSBlocks = -1;
+    self.matrix = nil;
 end
 
-function qrcode:Parse(str)
-    
+--get mode of the QRCode
+function qrcode:GetMode()
+    return self.mode;
 end
 
+--set mode of the QRCode
+function qrcode:SetMode(mode)
+    self.mode = mode
+end
 
+--get error correction level of the QRCode
+function qrcode:GetECLevel()
+    return self.ecLevel;
+end
+
+--set error correction level of the QRCode
+function qrcode:SetSCLevel(value)
+    self.ecLevel = value;
+end
+
+--get version of the QRCode, the bigger version, the bigger size
+function qrcode:GetVersion()
+    return self.version
+end
+
+--set version of the QRCode
+function qrcode:SetVersion(value)
+    self.version = value;
+end
+
+--get bytesMatrix width of the QRCode
+function qrcode:GetMatrixWidth()
+    return self.matrixWidth
+end
+
+--set bytesMatrix width of the QRCode
+function qrcode:SetMatrixWidth(value)
+    self.matrixWidth = value
+end
+
+--get Mask pattern of the QRCode
+function qrcode:GetMaskPattern()
+    return self.maskPattern
+end
+
+--check if "mask pattern" is vaild
+function qrcode:isValidMaskPattern(maskPattern)
+    return (maskPattern > 0 and maskPattern < NUM_MASK_PATTERNS)
+end
+
+--set mask pattern of the QRCode
+function qrcode:SetMaskPattern(value)
+    self.maskPattern = value
+end
+
+--get number of total bytes in the QRCode
+function qrcode:GetNumTotalBytes()
+    return self.numTotalBytes;
+end
+
+function qrcode:SetNumTotalBytes(value)
+    self.numTotalBytes = value
+end
+
+--get number of data bytes in the QRCode
+function qrcode:GetNumDataBytes()
+    return self.numDataBytes
+end
+
+function qrcode:SetNumDataBytes(value)
+    self.numDataBytes = value;
+end
+
+--get number of error correction in the QRCode
+function qrcode:GetNumECBytes()
+    return self.numECBytes;
+end
+
+function qrcode:SetNumECBytes(value)
+    self.numECBytes = value;
+end
+
+--get number of Reedsolomon blocks in the QRCode
+function qrcode:GetNumRSBlocks()
+    return self.numRSBlocks;
+end
+
+function qrcode:SetNumRSBlocks(value)
+    self.numRSBlocks = value;
+end
+
+--get ByteMatrix of the QRCode
+function qrcode:GetMatrix()
+    return self.matrix;
+end
+
+function qrcode:SetMatrix(value)
+    self.matrix = value
+end
+
+--Return the value of the module(cell) point by "x" and "y" in the matrix of the QRCode
+--They call cells in the matrix modules.
+--@result number  1 represents a black cell, and 0 represents a white cell
+function qrcode:at(x, y)
+    local value = self.matrix:get(x, y);
+    if not(value == 0 or value == 1) then
+        error("Matrix return value is bad.", 2);
+    end
+    return value
+end
+
+--Check all the member vars are set properly.
+--@resume boolean. true on success, otherwise returns false
+function qrcode:isVaild()
+
+end
+
+------------------------------------------------
+-- matrix class method
+-----------------------------------------------
 
 --[[
 test code
 ]]
 local barcode = LibStub("LibQRCode-1.0"):New();
-barcode:Create("http://www.wowace.com", "l");
-print(barcode.matrix)
+--barcode:Create("http://www.wowace.com", "l");
